ae78b8e362b468ed185c6bc9a3db18baca950c3b
javilopezleza <javilopezleza90@gmail.com>
2025-11-09 11:28:46 +0100

    Added hash strategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +12,10 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(),
+        // HashStrategy
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    },
   ]
 };
